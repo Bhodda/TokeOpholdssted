291d1981297653680553bfbee8e3a2b7daf1f6f2
Bhodda <christian.bonde.jensen@gmail.com>
2017-03-02 13:21:23 +0100

    1. draft
    on and off casual work for a couple weeks

---
+++ script/map.js
@@ -0,0 +1,22 @@
+function initMap() {
+    var location = {lat: 56.1095392, lng: 10.1840729};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: location,
+        disableDefaultUI: true
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
